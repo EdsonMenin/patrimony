4d8a2e906e524f3b6e6d26f3ee4d257c401018fb
meme <meme.menin@gmail.com>
2022-03-14 20:29:22 -0300

    1° commit

---
+++ backend/src/main/java/br/upf/produto/WsPatrimonyApplication.java
@@ -0,0 +1,13 @@
+package br.upf.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPatrimonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPatrimonyApplication.class, args);
+	}
+
+}
