780d2a833eb6620bf887a2277631b342584ac230
meme <meme.menin@gmail.com>
2022-03-21 20:13:29 -0300

    data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into tb_departament(name) values("sala de aula");
-insert into tb_departament(name) values("sala do professor");
+insert into tb_departament(name) values('sala de aula');
+insert into tb_departament(name) values('sala do professor');
